f9f15ffe6edb1f32cc9db022d1e20ab563684444
tidiview <francois.vidit@laposte.net>
2020-06-15 20:54:57 +0200

    initial commit

---
+++ src/routes/[lang].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export function preload (page) {
+    return { lang: page.params.lang };
+  }
+</script>
+
+<script>
+  export let lang;
+  const expressions = {
+      "fr": "Je t'aime",
+      "ja": "私は貴方を愛している",
+      "en": "I love you"
+    };
+  const expression = expressions[lang];
+</script>
+
+<svelte:head>
+  <title>{lang}</title>
+</svelte:head>
+
+<p>{expression}</p>
